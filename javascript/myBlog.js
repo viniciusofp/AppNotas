3d5607c50a3b1551734335dc5d60f89586b522e6
viniciusofp <viniciusofp@gmail.com>
2016-12-04 17:32:08 -0200

    test

--- javascript/myBlog.js
+++ javascript/myBlog.js
@@ -25,18 +25,30 @@ app.controller('BlogContrl', ['$scope', function($scope){
 }]);
 
 var archive = [{
-	
-	cor:"3",
-	createdAt:1480879288715,
-	imageUrl:"http://vaidape.com.br/wp-content/uploads/2016/11/05-704x420.jpg",
-	text:"Tá afim de aproveitar seu sábado com adrenalina? Se você curte um role de skate ou se amarra em ver as manobras da galera, não deixe de passar no CEU Butantã no sabadão,  dia 10 de dezembro.",
-	title:"CEU Butantã recebe festival de música e skate na pista do Butanclan"
+	title: 'My first even Post',
+	text: 'Hendrik\'s answer in the comments above will work but probably isn',
+	cor: 1,
+	imageUrl: 'http://bs.simplusmedia.com/i/730/838/banana-beneficios.jpg'
 },
 {
-	cor:"2",
-	createdAt:1480879345347,
-	imageUrl:"http://vaidape.com.br/wp-content/uploads/2016/11/MAR%C3%83IWATS%C3%89D%C3%89_02-6-1280x420.jpg",
-	text: "Na manhã desta quinta-feira (24), no Museu do Índio, no bairro de Botafogo, no Rio de Janeiro (RJ), aconteceu a abertura do Lançamento do Plano de Gestão da Terra Indígena Marãiwatsédé, retomada definitivamente pelo povo Xavante quase 50 anos depois da expulsão da população de seu território original.↵↵O Plano de Gestão é um documento que serve para melhorar a articulação e o diálogo dos Xavante com as instituições responsáveis pelo mapeamento, fiscalização e vigilância das terras. Além disso, publicação organiza e sistematiza um modelo produção e de vida que garanta a sustentabilidade e a diversidade ainda existente na região.",
-	title:"Marãiwatsédé: a história de luta do povo Xavante no Mato Grosso não terminou"
+	title: 'My first odd Post',
+	text: 'Hendrik\'s answer in the comments above will work but probably isn',
+	cor: 2,
+	imageUrl: 'http://www.saudedica.com.br/wp-content/uploads/2014/05/Benef%C3%ADcios-da-Ma%C3%A7%C3%A3.jpg'
+},
+{
+	title: 'My first odd Post',
+	text: 'Hendrik\'s answer in the comments above will work but probably isn',
+	cor: 3,
+	imageUrl: 'http://viniciusofp.com.br/wp-content/uploads/2016/03/olmoemcartazcapa-700x259.jpg'
+},
+{
+	title: 'My first odd Post',
+	text: 'Hendrik\'s answer in the comments above will work but probably isn',
+	cor: 4
+},
+{
+	title: 'My first odd Post',
+	text: 'Hendrik\'s answer in the comments above will work but probably isn',
+	cor: 5
 }];
-
